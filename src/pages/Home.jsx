24d28fb266d2344bfd5344cfa98234fa02a56cd8
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-11-17 07:23:46 +0530

    format file content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React, { lazy } from "react";
 const NavBar = lazy(() => import("../components/NavBar"));
+const Footer = lazy(() => import("../components/Footer"));
 
 const Home = () => {
   return (
     <React.Fragment>
       {/* header */}
       <NavBar />
+      {/* footer */}
+      <Footer />
     </React.Fragment>
   );
 };
