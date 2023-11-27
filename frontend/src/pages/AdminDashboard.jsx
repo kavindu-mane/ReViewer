2ee45df97680d0aef42528c5a4d3603443bb7222
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-11-27 16:41:42 +0530

    move react app to frontend directory

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -2,8 +2,10 @@ import React, { useState, lazy } from "react";
 const NavBar = lazy(() => import("../components/admin/NavBar.Admin"));
 const SideBar = lazy(() => import("../components/admin/SideBar.Admin"));
 const Books = lazy(() => import("../components/admin/Books.Manage.Admin"));
+
 const AdminDashboard = () => {
   const [isSideBarOpend, setIsSideBarOpend] = useState(false);
+  
   return (
     <React.Fragment>
       <div className="absolute start-0 top-0 z-0 flex h-screen min-h-[40rem] w-screen items-start">
