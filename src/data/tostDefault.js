bc677c042f74ee0958b555cbe5a2d561fc5f685e
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-12-04 18:45:10 +0530

    default toastify styles data file added

---
+++ src/data/tostDefault.js
@@ -0,0 +1,14 @@
+import { Zoom } from "react-toastify";
+
+const tostDefault = {
+  transition: Zoom,
+  position: "top-center",
+  autoClose: 3000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  theme: "colored",
+};
+
+export default tostDefault;
