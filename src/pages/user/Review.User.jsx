ea7d1dc2c7362660deaaaaf95803cd7993b0460f
Gimhani2000 <86472955+Gimhani2000@users.noreply.github.com>
2024-01-06 16:30:00 +0530

    Update ReviewUser.jsx

--- src/pages/user/Review.User.jsx
+++ src/pages/user/Review.User.jsx
@@ -5,6 +5,8 @@ import BookCard from "../../components/user/BookCard";
 import StarRating from "../../components/user/StarRating";
 import AddReview from "../../components/user/AddReview";
 import CustomerReview from "../../components/user/CustomerReview";
+import BookDetails from "../../components/user/BookDetails";
+import WishList from "../../components/user/WishList";
 
 const Review = () => {
   const mainDivStyle = {
@@ -19,32 +21,43 @@ const Review = () => {
       <div className="relative flex min-h-screen flex-col justify-between" >
         {/* header */}
         <NavBar />
-        <section class="bg-white dark:bg-gray-900">
-          <div class="py-8 px-4 mx-auto max-w-screen-auto lg:py-16 lg:px-6" style={mainDivStyle}>
-            <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-              <BookCard />
+        {/* Body */}
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-auto lg:py-16 lg:px-6" >
+          <div className="grid grid-rows grid-flow-col gap-10">
+          {/* Left column */}
+          <div className="row-span-3 ...">
+            <BookCard />
+            <br></br>
+            <WishList />
+          </div>
+          {/* Top row */}
+          <div className="col-span-2 ...">     
+              <BookDetails/>
               <br></br>
               <StarRating />
-              <br></br>
+              <br></br>          
               <AddReview />
-            </div>
-            <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-              <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-Black rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-                <CustomerReview />
-              </div>
-              <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-Black rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-                <CustomerReview />
-              </div>
-              <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-Black rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-                <CustomerReview />
-              </div>
-            </div>
+              <br></br>
+              <hr class="h-px my-8 bg-gray-400 border-0 dark:bg-gray-700"></hr>
           </div>
-        </section>
-
-        {/* footer */}
-        <Footer />
+          {/* Bottom row */}
+          <div className="row-span-2 col-span-2 ..."> 
+          <div className="space-y-8 lg:grid lg:grid-raw-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+          <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">User Reviews</h3>
+                <CustomerReview />
+                <br></br>
+                <CustomerReview />
+                <br></br>
+                <CustomerReview />
+          </div>
+          </div>
+        </div>
       </div>
+      </section>
+    {/* footer */}
+     <Footer />
+    </div>
     </React.Fragment>
   );
 };
