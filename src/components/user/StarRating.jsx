b908172ddb41dda4556b2165d0a1fb1e78ea6a68
Gimhani2000 <86472955+Gimhani2000@users.noreply.github.com>
2024-01-06 16:21:44 +0530

    Update Starrating.jsx

--- src/components/user/StarRating.jsx
+++ src/components/user/StarRating.jsx
@@ -4,18 +4,16 @@ import { Card } from 'flowbite-react';
 
 function StarRating() {
   return (
-    <div>
-    <Card className="max-w-sm">      
-        <Rating>
-      <Rating.Star />
-      <Rating.Star />
-      <Rating.Star />
-      <Rating.Star />
-      <Rating.Star filled={false} />
-      <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">4.95 out of 5</p>
-    </Rating>  
-    </Card>
-  </div>
+<div class="flex items-center">
+    <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+    </svg>
+    <p class="ms-2 text-sm font-bold text-gray-900 dark:text-white">4.95</p>
+    <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+    <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">73 reviews</a>
+</div>
+
+
   )
 }
 
